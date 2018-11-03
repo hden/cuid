f40abb1e183c46235e54188d1e17d5aac1aff15c
Haokang Den <haokang.den@gmail.com>
2018-11-03 10:54:50 +0900

    add test coverage

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :global-vars {*warn-on-reflection* true})
+  :global-vars {*warn-on-reflection* true}
+  :plugins [[lein-cloverage "1.0.13"]])
