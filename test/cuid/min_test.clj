087a8243941c25e7ff606156da71c44723f7ed2c
Haokang Den <haokang.den@gmail.com>
2018-11-03 11:41:27 +0900

    Fix custom fingerprint.

--- test/cuid/min_test.clj
+++ test/cuid/min_test.clj
@@ -6,5 +6,8 @@
   (testing "Constant result can be obtained for testing by replacing all random functions"
     (is (= "cj3n55xem0000000000000000" (cuid {:timestamp    (constantly 1496848861534)
                                               :counter      (constantly 0)
-                                              :fingerprint  (constantly "")
-                                              :random-block (constantly "")})))))
+                                              :fingerprint  (constantly 0)
+                                              :random-block (constantly "")}))))
+
+  (testing "Minimal parameter."
+    (is (cuid {}))))
